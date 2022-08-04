90a194d55c8eb5116dc64798f987ae7602c8cf48
Jakub <jakub.matwis@gmail.com>
2022-08-04 13:15:28 +0200

    Hero movie overview done

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,18 +15,46 @@ export const Main = () => {
         setMovies(response.data.results);
       });
   }, []);
-  const randomMovie =
+  const heroMovie =
     movies[
       Math.floor(Math.random() * movies.length)
     ];
+  const truncateString = (string, maxLength) =>
+    string?.length > maxLength
+      ? string.slice(0, maxLength) + "..."
+      : string;
 
   return (
     <div className="w-full h-[550px] text-white">
       <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
         <img
-          src={`https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}`}
-          alt={randomMovie?.title}
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${heroMovie?.backdrop_path}`}
+          alt={heroMovie?.title}
         />
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h2 className="text-3xl md:text-5xl font-bold">
+            {heroMovie?.title}
+          </h2>
+          <div className="my-4">
+            <button className="border bg-gray-300 border-gray-300 text-black py-2 px-5">
+              Play
+            </button>
+            <button className="border  border-gray-300 text-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+          <p className="text-gray-400 text-sm">
+            Relased: {heroMovie?.relase_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(
+              heroMovie?.overview,
+              200
+            )}
+          </p>
+        </div>
       </div>
     </div>
   );
