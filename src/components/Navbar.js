33c49fd823925f728c3d8d9999ed8843a2afdab0
Jakub <jakub.matwis@gmail.com>
2022-08-09 21:29:44 +0200

    Login and Sign Up components marked up and styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,12 +10,16 @@ export const Navbar = () => {
         </h1>
       </Link>
       <div>
-        <button className="pr-4 text-white">
-          Sign In
-        </button>
-        <button className="px-6 py-2 text-white bg-red-600 rounded cursor-pointer">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="pr-4 text-white">
+            Sign In
+          </button>
+        </Link>
+        <Link to="signup">
+          <button className="px-6 py-2 text-white bg-red-600 rounded cursor-pointer">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </nav>
   );
